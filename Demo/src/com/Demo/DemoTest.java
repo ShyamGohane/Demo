1fc31ad1f127ccbe6f254b3fbbfd952986e8f7bd
Your Name <you@example.com>
2022-12-22 14:06:49 +0530

    add my demo file

---
+++ Demo/src/com/Demo/DemoTest.java
@@ -0,0 +1,12 @@
+package com.Demo;
+
+import java.util.ArrayList;
+
+public class DemoTest {
+public static void main(String[] args) {
+	ArrayList al=new ArrayList();
+	al.add(12);
+	al.add(25);
+	System.out.println(al);
+}
+}
